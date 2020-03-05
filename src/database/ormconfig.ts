35630cbb0b3226c51d7e00d733a9ff2592461989
foreshadow <MrJesper@yandex.ru>
2020-03-05 22:17:20 +0600

    fixing erorr with ormconfig.ts

--- src/database/ormconfig.ts
+++ src/database/ormconfig.ts
@@ -12,7 +12,7 @@ const config: ConnectionOptions = {
   migrationsRun: true,
   logging: true,
   logger: 'file',
-  migrations: [__dirname + './migrations/**/*{.ts,.js}'],
+  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
   cli: {
     migrationsDir: 'src/migrations',
   },
